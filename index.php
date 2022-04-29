d6ad80b3e3e2aa7a13278347df11adf85174f338
Zoms <martinezoms15@gmail.com>
2022-04-29 13:57:18 +0100

    project ready

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+require('./user_validator.php');
+
+
+if (isset($_POST['submit'])) {
+  //Validate entries
+  $validation = new UserValidator($_POST);
+  $errors = $validation->validateForm();
+
+  // save data to database
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Object & Classes</title>
+</head>
+
+<body>
+  <div class="new-user">
+    <h2>Create a new user</h2>
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+
+      <label>username: </label>
+      <input type="text" name="username" value="<?php echo htmlspecialchars($_POST['username']) ?? '' ?>">
+      <div class="error">
+        <?php
+        echo $errors['username'] ?? ''
+        ?>
+      </div>
+
+      <label>email: </label>
+      <input type="text" name="email" value="<?php echo htmlspecialchars($_POST['email']) ?? '' ?>">
+      <div class="error">
+        <?php
+        echo $errors['email'] ?? ''
+        ?>
+      </div>
+
+      <input type="submit" value="submit" name="submit">
+
+    </form>
+  </div>
+</body>
+
+</html>
